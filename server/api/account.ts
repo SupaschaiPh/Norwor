515e370443f33583a8ebab8a080295c24bb0fd4c
Boszsp <112678332+SupaschaiPh@users.noreply.github.com>
2024-04-09 04:55:29 +0700

    add : kick account api

--- server/api/account.ts
+++ server/api/account.ts
@@ -1,18 +1,38 @@
-import { tables, useDrizzle } from "../utils/drizzle"
+import Database from "better-sqlite3";
+import { tables, useDrizzle } from "../utils/drizzle";
 
 export default defineEventHandler(async (event) => {
-  const config = await useRuntimeConfig()
-  let mss = ""
-  let statusCode = 200
-  const session = await useSession(event,{
-    password:config.SECRETKEY
-  })
-  let users = (await useDrizzle().select().from(tables.users).all()).map((v)=>{ v.password = "" ;return v})
-     
-    return {
-      status:statusCode,
-      mss,
-      body:users
+  const config = await useRuntimeConfig();
+  let mss = "";
+  let statusCode = 200;
+  let users: Database.RunResult | Object | null = null;
+  const session = await useSession(event, {
+    password: config.SECRETKEY,
+  });
+
+  if (event.method == "DELETE") {
+    const body = await readBody(event);
+    if (body && body.id) {
+      users = await useDrizzle()
+        .delete(tables.users)
+        .where(eq(tables.users.id, body.id));
+      mss = "success";
     }
-  })
-  
+
+    return {
+      status: statusCode,
+      mss,
+      body: users,
+    };
+  } else {
+    users = (await useDrizzle().select().from(tables.users).all()).map((v) => {
+      v.password = "";
+      return v;
+    });
+  }
+  return {
+    status: statusCode,
+    mss,
+    body: users,
+  };
+});
