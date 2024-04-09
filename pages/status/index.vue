5955fdd718d4ce734d8e0153141c462ff8a94d0a
Boszsp <112678332+SupaschaiPh@users.noreply.github.com>
2024-04-09 20:52:20 +0700

    add : rmtp stat table

--- pages/status/index.vue
+++ pages/status/index.vue
@@ -1,8 +1,13 @@
 <script setup>
 import colors from "vuetify/util/colors";
+import convert from "xml-js";
+import rmtpStatusTable from "./rmtpStatusTable.vue";
 
-const isRail = useIsRail()
-isRail.value = false
+const isRail = useIsRail();
+const rtmpStat = ref(null);
+const isAppAsArray = ref(false);
+
+isRail.value = false;
 
 const status = ref([
   {
@@ -21,6 +26,40 @@ const status = ref([
     status: "ok",
   },
 ]);
+onMounted(() => {
+  $fetch("https://itcdev.jarukrit.net/raw_stats/").then((res) => {
+    rtmpStat.value = convert.xml2js(res, {
+      compact: true,
+      nativeType: true,
+    }).rtmp;
+    console.log(
+      convert.xml2json(res, { compact: true, nativeType: true, spaces: 4 })
+    );
+    //console.log(typeof rtmpStat.value.server.application.live.stream.client)
+    isAppAsArray.value = rtmpStat.value.server.application.name == undefined;
+    if (
+      !isAppAsArray.value &&
+      rtmpStat.value.server.application.live.stream.client.address == undefined
+    ) {
+      console.log("I am array");
+      rtmpStat.value.server.application.live.stream.client =
+        rtmpStat.value.server.application.live.stream.client.filter((v) => {
+          "publishing" in v;
+        })[0];
+    } else if (isAppAsArray.value) {
+      rtmpStat.value.server.application = rtmpStat.value.server.application.map(
+        (v) => {
+          if (v.live.stream.client.address == undefined) {
+            v.live.stream.client = v.live.stream.client.filter((v) => {
+              "publishing" in v;
+            })[0];
+          }
+          return v
+        }
+      );
+    }
+  });
+});
 </script>
 
 <template>
@@ -47,12 +86,13 @@ const status = ref([
           </v-card-item>
         </v-card>
         <span v-for="statu in status">
-          <statusCard
-            :title="statu.title"
-            :status_code="statu.status_code"
-          ></statusCard>
+          <statusCard :title="statu.title" :status_code="statu.status_code">
+          </statusCard>
         </span>
       </v-card-text>
     </v-card>
+    <rmtpStatusTable :isAppAsArray="isAppAsArray" :rtmp-stat="rtmpStat" />
+
+    {{ JSON.stringify(rtmpStat) }}
   </div>
 </template>
---
+++ pages/status/rmtpStatusTable.vue
@@ -0,0 +1,172 @@
+<script setup>
+defineProps({
+  rtmpStat: Object,
+  isAppAsArray:Boolean
+});
+</script>
+<template>
+  <v-card>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">Bandwidth In</th>
+          <th class="text-left">Bandwidth Out</th>
+          <th class="text-left">Bytes In</th>
+          <th class="text-left">Bytes Out</th>
+          <th class="text-left">Uptime</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ rtmpStat?.bw_in._text }}</td>
+          <td>{{ rtmpStat?.bw_out._text }}</td>
+          <td>{{ rtmpStat?.bytes_in._text }}</td>
+          <td>{{ rtmpStat?.bytes_out._text }}</td>
+          <td>{{ rtmpStat?.uptime._text }}</td>
+        </tr>
+        <tr>
+          <th colspan="5">Server Application</th>
+        </tr>
+        <tr>
+          <td colspan="5">
+            <v-card class="m-4">
+              <v-table>
+                <thead>
+                  <tr>
+                    <th class="text-left">Stream Key</th>
+                    <th class="text-left">Time</th>
+                    <th class="text-left">Bandwidth In</th>
+                    <th class="text-left">Bandwidth Out</th>
+                    <th class="text-left">Bytes In</th>
+                    <th class="text-left">Bytes Out</th>
+                    <th class="text-left">Bandwidth Video</th>
+                    <th class="text-left">Bandwidth Audio</th>
+                  </tr>
+                </thead>
+                <tbody v-if="!isAppAsArray">
+                  <tr >
+                    <td>
+                      {{ rtmpStat?.server.application.live.stream.name._text }}
+                    </td>
+                    <td>
+                      {{ rtmpStat?.server.application.live.stream.time._text }}
+                    </td>
+                    <td>
+                      {{ rtmpStat?.server.application.live.stream.bw_in._text }}
+                    </td>
+                    <td>
+                      {{
+                        rtmpStat?.server.application.live.stream.bw_out._text
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        rtmpStat?.server.application.live.stream.bytes_in._text
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        rtmpStat?.server.application.live.stream.bytes_out._text
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        rtmpStat?.server.application.live.stream.bw_video._text
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        rtmpStat?.server.application.live.stream.bw_audio._text
+                      }}
+                    </td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-card>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="5">Client Info</th>
+        </tr>
+        <tr>
+          <td colspan="5">
+            <v-card class="m-4">
+              <v-table>
+                <thead>
+                  <tr>
+                    <th class="text-left">Id</th>
+                    <th class="text-left">Address</th>
+                    <th class="text-left">Time</th>
+                    <th class="text-left">Flashver</th>
+                    <th class="text-left">Swf URL</th>
+                    <th class="text-left">Dropped</th>
+                    <th class="text-left">Avsync</th>
+                    <th class="text-left">Timestamp</th>
+                  </tr>
+                </thead>
+                <tbody v-if="!isAppAsArray">
+                  <tr>
+                    <td>
+                      {{ rtmpStat?.server.application.live.stream.client.id._text }}
+                    </td>
+                    <td>
+                      {{ rtmpStat?.server.application.live.stream.client.address._text }}
+                    </td>
+                    <td>
+                      {{ rtmpStat?.server.application.live.stream.client.time._text }}
+                    </td>
+                    <td>
+                        {{ rtmpStat?.server.application.live.stream.client.flashver._text }}
+                    </td>
+                    <td>
+                        {{ rtmpStat?.server.application.live.stream.client.swfurl._text }}
+                    </td>
+                    <td>
+                        {{ rtmpStat?.server.application.live.stream.client.dropped._text }}
+                    </td>
+
+                    <td>
+                        {{ rtmpStat?.server.application.live.stream.client.avsync._text }}
+                    </td>
+                    <td>
+                        {{ rtmpStat?.server.application.live.stream.client.timestamp._text }}
+                    </td>
+                  </tr>
+                </tbody>
+                <tbody v-if="isAppAsArray">
+                    <tr v-for="application in rtmpStat?.server.application" :key="application.live.stream.client.id">
+                    <td>
+                      {{ application.live.stream.client.id._text }}
+                    </td>
+                    <td>
+                      {{ application.live.stream.client.address._text }}
+                    </td>
+                    <td>
+                      {{ application.live.stream.client.time._text }}
+                    </td>
+                    <td>
+                        {{ application.live.stream.client.flashver._text }}
+                    </td>
+                    <td>
+                        {{ application.live.stream.client.swfurl._text }}
+                    </td>
+                    <td>
+                        {{ application.live.stream.client.dropped._text }}
+                    </td>
+
+                    <td>
+                        {{ application.live.stream.client.avsync._text }}
+                    </td>
+                    <td>
+                        {{ application.live.stream.client.timestamp._text }}
+                    </td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-card>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-card>
+</template>
