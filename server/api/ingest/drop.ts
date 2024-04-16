fcc76779a4e3dc9cac4507007e35b5b7dd17ece8
Jarukrit <jarukritsripaploen@protonmail.com>
2024-04-17 00:37:39 +0700

    Chore: refactor `drop.ts`

--- server/api/ingest/drop.ts
+++ server/api/ingest/drop.ts
@@ -5,25 +5,33 @@ export default defineEventHandler(async (event) => {
   const session = await useSession(event, {
     password: config.SECRETKEY,
   });
-  const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const characters =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
   console.log(session.data);
   if (session.data.acc.username === "admin") {
     if ("stream_id" in params && "app" in params && "ban" in params) {
-      $fetch(
-        `http://localhost/control/drop/publisher?app=${params.app}&name=${params.stream_id}`
-      ).then(async (res) => {
+      $fetch(`http://localhost/control/drop/publisher`, {
+        query: {
+          app: params.app,
+          name: params.stream_id,
+        },
+      }).then(async (res) => {
         if (res === 1) {
           if (params.ban === "true" && params.app !== "live") {
-            const updateRes = await useDrizzle()
+            await useDrizzle()
               .update(tables.videos)
               .set({
-                stream_id: Array.from(crypto.getRandomValues(new Uint32Array(8)))
-                .map((x) => characters[x % characters.length])
-                .join(""),
-                stream_key: Array.from(crypto.getRandomValues(new Uint32Array(20)))
-                .map((x) => characters[x % characters.length])
-                .join(""),
+                stream_id: Array.from(
+                  crypto.getRandomValues(new Uint32Array(8))
+                )
+                  .map((x) => characters[x % characters.length])
+                  .join(""),
+                stream_key: Array.from(
+                  crypto.getRandomValues(new Uint32Array(20))
+                )
+                  .map((x) => characters[x % characters.length])
+                  .join(""),
               })
               .where(eq(tables.videos.id, 1));
             return {
